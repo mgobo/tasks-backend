e1d0b1bc4784c19542e20f0337b03b41d786272f
Mateus Gobo <mateusgobo@MacBook-Pro-de-Mateus-Gobo-2.local>
2021-09-22 10:14:24 -0300

    Ajustando porta

--- src/main/docker/jenkins.sh
+++ src/main/docker/jenkins.sh
@@ -12,7 +12,6 @@ docker run --name jenkins \
 docker run --name jenkins \
 --privileged \
 -v /Users/mateusgobo/dockerdata/jenkins_home:/var/jenkins_home \
--v /Users/mateusgobo/apache-tomcat-8.5.50:/var/jenkins_home/workspace/apache \
--p 9100:8080 \
+-p 9100:9001 \
 -p 9101:50000 \
--d jenkins/jenkins:alpine
+-d jenkins:2.313-alpine-jdk8
